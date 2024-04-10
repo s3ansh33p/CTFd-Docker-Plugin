287b407213648bbc79745eaea506b1d0ce1670e2
Sean McGinty <31000120+s3ansh33p@users.noreply.github.com>
2024-04-10 19:39:27 +0800

    Update view.js

--- assets/view.js
+++ assets/view.js
@@ -1,12 +1,6 @@
 CTFd._internal.challenge.data = undefined;
 
-CTFd._internal.challenge.renderer = CTFd.lib.markdown();
-
 CTFd._internal.challenge.preRender = function () {};
-
-CTFd._internal.challenge.render = function (markdown) {
-	return CTFd._internal.challenge.renderer.render(markdown);
-};
 
 CTFd._internal.challenge.postRender = function () {};
 
